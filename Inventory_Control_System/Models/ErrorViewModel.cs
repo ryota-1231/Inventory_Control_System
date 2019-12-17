26956e1ef1159b67b08db9afe41b3f4839a8b30e
ryota-1231 <rrrrr.1231@gmail.com>
2019-12-17 22:25:16 +0900

    プロジェクト作成

---
+++ Inventory_Control_System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Inventory_Control_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
